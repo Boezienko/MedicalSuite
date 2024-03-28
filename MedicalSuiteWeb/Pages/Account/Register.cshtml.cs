6fc47e93d852cb4629b1aeace89181d5411066f2
Boezienko <boeazienko@gmail.com>
2024-03-28 14:41:04 -0500

    made basic pages, login, profile, and register.

---
+++ MedicalSuiteWeb/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MedicalSuiteWeb.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
