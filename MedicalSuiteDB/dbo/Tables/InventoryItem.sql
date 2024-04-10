7778118d6e0f89b78aab8693e95a3e772e488c81
0re0luwa <Dammiiee2003@gmail.com>
2024-04-10 13:17:13 -0500

    Add item page
    Created an add item page and formatted it. Also created a new table in the database called Inventoryitem

---
+++ MedicalSuiteDB/dbo/Tables/InventoryItem.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[InventoryItem] (
+    [InventoryItemId]          INT            NOT NULL,
+    [InventoryItemCode]        VARCHAR (5)    NOT NULL,
+    [InventoryItemName]        VARCHAR (100)  NOT NULL,
+    [InventoryItemDescription] VARCHAR (250)  NOT NULL,
+    [InventoryItemPrice]       DECIMAL (5, 2) NOT NULL,
+    [CategoryId]               INT            IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_InventoryItem] PRIMARY KEY CLUSTERED ([InventoryItemId] ASC),
+    CONSTRAINT [FK_InventoryItem_InventoryItem] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([CategoryId])
+);
+
---
+++ MedicalSuiteWeb/Model/InventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalSuiteWeb.Model
+{
+    public class InventoryItem
+    {
+        public int InventoryItemId { get; set; }
+        [Required]
+        [Display(Name = "Item Code")]
+        public string InventoryItemCode { get; set; }
+        [Display(Name = "Item Name")]
+        public string InventoryItemName { get; set; }
+        [Display(Name = "Description")]
+        public string InventoryItemDescription { get; set;}
+        [Display(Name = "Price")]
+        public decimal InventoryItemPrice { get; set; }
+        [Display(Name = "Category")]
+        public int CategoryId {  get; set; }
+    }
+}
