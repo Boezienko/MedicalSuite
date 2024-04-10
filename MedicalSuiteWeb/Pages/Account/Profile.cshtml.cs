86d63c46a9fdab3662ac01c7815f07a60042845f
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2024-04-10 13:17:13 -0500

    Added Profile.html

--- MedicalSuiteWeb/Pages/Account/Profile.cshtml.cs
+++ MedicalSuiteWeb/Pages/Account/Profile.cshtml.cs
@@ -5,6 +5,9 @@ namespace MedicalSuiteWeb.Pages.Account
 {
     public class ProfileModel : PageModel
     {
+        [BindProperty]
+        public UserProfile profile {  get; set; }
+
         public void OnGet()
         {
         }
