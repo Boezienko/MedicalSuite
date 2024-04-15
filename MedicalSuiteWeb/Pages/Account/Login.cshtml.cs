927a8036a4b63c9de65bad251e00ea05d9f8d97d
0re0luwa <Dammiiee2003@gmail.com>
2024-04-15 11:17:17 -0500

    Work done in class

--- MedicalSuiteWeb/Pages/Account/Login.cshtml.cs
+++ MedicalSuiteWeb/Pages/Account/Login.cshtml.cs
@@ -15,20 +15,12 @@ namespace MedicalSuiteWeb.Pages.Account
         }
         public ActionResult OnPost()
         {
-            
+
             if (ModelState.IsValid)
-            {   
-                SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString());
-                string cmdText = "SELECT PasswordHash FROM Person WHERE Email=@email";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-
-                // Add the @email parameter
-                cmd.Parameters.AddWithValue("@email", LoginUser.Email);
-
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if(reader.HasRows)
+            {
+                if (ValidateCredentials())
                 {
+<<<<<<< Updated upstream
                     reader.Read();
                     if (!reader.IsDBNull(0))
                     {
@@ -47,11 +39,12 @@ namespace MedicalSuiteWeb.Pages.Account
                     {
                         return Page();
                     }
+=======
+                    return Redirect
+>>>>>>> Stashed changes
                 }
-                conn.Close();
-                return RedirectToPage("Profile");
-
-            } else
+            }
+            else
             {
                 return Page();
             }
@@ -62,7 +55,8 @@ namespace MedicalSuiteWeb.Pages.Account
         {
             using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
             {
-                string cmdText = "SELECT PasswordHash, PersonId FROM Person WHERE Email=@email";
+                string cmdText = "SELECT PasswordHash, PersonId, FirstName, Email FROM Person " +
+                    " INNER JOIN [Role] ON Person.RoleId = [Role].RoleId WHERE Email=@email";
                 SqlCommand cmd = new SqlCommand(cmdText, conn);
 
                 // Add the @email parameter
@@ -84,6 +78,29 @@ namespace MedicalSuiteWeb.Pages.Account
                             //Get the PersonId, and use it to update the Person record
                             int personId = reader.GetInt32(1);
                             UpdatePersonLoginTime(personId);
+
+                            // Create a principle
+                            string name = reader.GetString(2);
+                            string roleName = reader.GetString(4);
+
+                            // Create a list of Claims
+                            Claim emailClaim = new Claim(ClaimTypes.Email, LoginUser.Email);
+                            Claim nameClaim = new Claim(ClaimTypes.Name, name);
+                            Claim roleClaim = new Claim(ClaimTypes.Role, roleName);
+
+                            List<Claim> claims = new List<Claim> { emailClaim, nameClaim, roleClaim };
+
+                            // Add the list of claims to ClaimIdentity
+                            ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                            // 3. Add the identity to a ClaimsPrinciple
+                            ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+                            // 4. Call HttpContext.SigninAsync() method to encrypt the principal
+                            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+
+
                             return true;
                         }
                         else
@@ -92,7 +109,10 @@ namespace MedicalSuiteWeb.Pages.Account
                         }
                     
                     }
+<<<<<<< Updated upstream
                     
+=======
+>>>>>>> Stashed changes
 
                 }
                 else
--- MedicalSuiteWeb/Pages/Account/Profile.cshtml.cs
+++ MedicalSuiteWeb/Pages/Account/Profile.cshtml.cs
@@ -1,6 +1,12 @@
 using MedicalSuiteWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+<<<<<<< Updated upstream
+=======
+using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Security.Claims;
+>>>>>>> Stashed changes
 
 namespace MedicalSuiteWeb.Pages.Account
 {
@@ -10,7 +16,30 @@ namespace MedicalSuiteWeb.Pages.Account
         public UserProfile profile { get; set; }
         public void OnGet()
         {
-
+            PopulateProfile();
         }
+<<<<<<< Updated upstream
+=======
+
+        private void PopulateProfile()
+        {
+            //Query the person table to populate "profile" object
+
+            string email = HttpContext.User.FindFirstValue(ClaimValueTypes.Email);
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                string cmdText = "SELECT FirstName, LastName, Email, Phone, LastLoginTime FROM Person WHERE Email=@email";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@email", email);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    profile.FirstName = reader.GetString(0);
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
