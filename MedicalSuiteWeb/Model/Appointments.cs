95076bed1d52a9631fddeac201edefe5e25b43f7
0re0luwa <Dammiiee2003@gmail.com>
2024-04-24 09:42:34 -0500

    Made Appointments
    Made the database table of appointments, mad the appointment model and the html and html.cs files and customized them

--- MedicalSuiteWeb/Model/Appointments.cs
+++ MedicalSuiteWeb/Model/Appointments.cs
@@ -1,6 +1,18 @@
-﻿namespace MedicalSuiteWeb.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalSuiteWeb.Model
 {
     public class Appointments
     {
+        public int AppointmentId {  get; set; }
+        [Required]
+        [Display(Name = "Appointment Date")]
+        public DateTime AppointmentDate { get; set; }
+        [Required]
+        [Display(Name = "Appointment Time")]
+        public TimeSpan AppointmentTime { get; set;}
+        public string AppointmentNotes { get; set;}
+        public string DoctorsName { get; set; }
+        public int PersonId { get; set; }
     }
 }
