d578b45f41aa59f7003fbfcb2152d490747141df
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2024-04-26 03:04:36 -0500

    scuffed push, wip
    just pushing for progress, implementing a public static method for deciding layouts is coming along well. At first I was just passing the string to it but now i think i want to pass the httpcontext along too to make it more concise.

---
+++ MedicalSuiteWeb/Pages/LayoutAuthorizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+
+namespace MedicalSuiteWeb.Pages
+{
+    public class LayoutAuthorizer
+    {
+        static public string GetLayoutFromRole(string userRole)
+        {
+            string layout = String.Empty;
+            if (userRole.Equals("Patient"))
+            {
+                layout = "_LayoutPatient"; // Layout for patients
+            }
+            else if (userRole.Equals("Nurse"))
+            {
+                layout = "_LayoutNurse"; // Layout for nurses
+            }
+            else if (userRole.Equals("Doctor"))
+            {
+                layout = "_LayoutDoctor"; // Layout for doctors
+            }
+            return layout;
+        }
+    }
+}
+
