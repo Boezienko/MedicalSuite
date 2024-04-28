f129ceee0ef91da3370c433e592cf63c149917d2
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2024-04-28 16:35:43 -0500

    finished appointment functionality for now
    added appointment functionality at the patient level, still has to be implemented at the doctor and nurse level

--- MedicalSuiteWeb/Pages/Appointments/EditAppointment.cshtml.cs
+++ MedicalSuiteWeb/Pages/Appointments/EditAppointment.cshtml.cs
@@ -1,12 +1,37 @@
+using MedicalSuiteBusiness;
+using MedicalSuiteWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace MedicalSuiteWeb.Pages.Appointments
 {
+    [BindProperties]
     public class EditAppointmentModel : PageModel
     {
-        public void OnGet()
+        public Appointment specifiedAppointment { get; set; } = new Appointment();
+        public void OnGet(int id)
         {
+            PopulateAppointment(id);
+        }
+
+        private void PopulateAppointment(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                string cmdText = "SELECT AppointmentId, AppointmentDate, AppointmentTime FROM Appointments WHERE AppointmentId=@appointmentId";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                cmd.Parameters.AddWithValue("@appointmentId", id);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    specifiedAppointment.AppointmentId = id;
+                    specifiedAppointment.AppointmentDate = reader.GetDateTime(1);
+                    specifiedAppointment.AppointmentTime = reader.GetTimeSpan(2);
+                }
+            }
         }
     }
 }
