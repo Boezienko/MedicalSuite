b2ffb725763b55c6945ef17079dfda9854cce111
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2024-04-29 15:13:24 -0500

    updated appointments.sql

--- MedicalSuiteDB/dbo/Tables/Appointments.sql
+++ MedicalSuiteDB/dbo/Tables/Appointments.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Appointments] (
-    [AppointmentId]    INT           IDENTITY (1, 1) NOT NULL,
-    [AppointmentDate]  DATETIME      NOT NULL,
-    [AppointmentTime]  TIME (7)      NOT NULL,
-    [AppointmentNotes] VARCHAR (250) NULL,
-    [DoctorsName]      VARCHAR (50)  NOT NULL,
-    [PersonId]         INT           NOT NULL,
-    CONSTRAINT [PK_Appointments] PRIMARY KEY CLUSTERED ([AppointmentId] ASC),
-    CONSTRAINT [FK_Appointments_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId])
+    [AppointmentId]   INT      IDENTITY (1, 1) NOT NULL,
+    [AppointmentDate] DATE     NOT NULL,
+    [AppointmentTime] TIME (7) NOT NULL,
+    [PersonId]        INT      NOT NULL,
+    CONSTRAINT [FK_Appointments_Person] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId]),
+    CONSTRAINT [PK_Appointments] PRIMARY KEY CLUSTERED ([AppointmentId] ASC)
 );
+
+
 
